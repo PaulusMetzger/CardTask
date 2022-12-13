e6af970d2802263a97954a939bf2791205f29e25
Pavel <paulusalex@mail.ru>
2022-12-13 21:34:04 +0300

    Add fields in card script

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private TMP_Text mana;
+    [SerializeField] private TMP_Text health;
+    [SerializeField] private TMP_Text attack;
 }
